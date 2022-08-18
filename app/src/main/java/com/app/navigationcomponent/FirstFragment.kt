c3d8ce64e612428a681152d5edb1d31f07821ba3
saleh rashid <saleh.rashid.b@gmail.com>
2022-08-18 11:03:48 +0700

    initial commit

---
+++ app/src/main/java/com/app/navigationcomponent/FirstFragment.kt
@@ -0,0 +1,29 @@
+package com.app.navigationcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //non action
+        val button = view.findViewById<Button>(R.id.btn_1)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.secondFragment2)
+        }
+    }
+}
---
+++ app/src/main/java/com/app/navigationcomponent/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.app.navigationcomponent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.my_nav_host_fragement
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+}
